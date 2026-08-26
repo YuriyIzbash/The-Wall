a841e442998696eb05a03b4113151b6830ea3cd3
Yuriy Izbash <izbash91@gmail.com>
2026-08-26 17:05:52 +0200

    Added new fonts and colors

--- src/utils/graffitiStyles.js
+++ src/utils/graffitiStyles.js
@@ -6,6 +6,36 @@ const fonts = [
   'Jraot Hollow, sans-serif',
   'Docallisme on Street, cursive',
   'A Dripping Marker, cursive',
+  'DropShade-Melt, cursive',
+  'FankyBubbleGraffiti-Line, cursive',
+  'Ghoust Solid, cursive',
+  'Graffiti Treat, cursive',
+  'GraffitiNewDemoRegular, cursive',
+  'Kortz DEMO, cursive',
+  'MARKER-NO2HUN, cursive',
+  'MARSNEVENEKSK-Regular, cursive',
+  'Mersey Cowboy, cursive',
+  'More Than Life, cursive',
+  'NightclawVandalReg-Solid, cursive',
+  'No_License_Ghang, cursive',
+  'NoctraDrip-SolidMelt, cursive',
+  'ONE PIECE, cursive',
+  'Painterz, cursive',
+  'Rebeland Regular, cursive',
+  'RockerRusher, cursive',
+  'RollbeatGraffiti, cursive',
+  'SoachaLetterFont-Regular, cursive',
+  'Spoken_PERSONAL, cursive',
+  'Step Better, cursive',
+  'Street Wars Demo, cursive',
+  'StreetartDemoRegular, cursive',
+  'THE_JACATRA, cursive',
+  'Throw-up Font, cursive',
+  'UrbanSketart, cursive',
+  'aAttackGraffiti, cursive',
+  'bboy, cursive',
+  'graffonti.atomic.bomb, cursive',
+  'sickcapital-vice, cursive',
 ];
 
 // Text colors
@@ -18,6 +48,25 @@ const textColors = [
   '#ff66ff',
   '#ff3399',
   '#ff6600',
+  '#ff0066',
+  '#ff3366',
+  '#ff9933',
+  '#66ff99',
+  '#00ccff',
+  '#cc66ff',
+  '#ff66cc',
+  '#ff9966',
+  '#99ff66',
+  '#66ffcc',
+  '#cc66cc',
+  '#ff3399',
+  '#ff6600',
+  '#ffcc66',
+  '#66ff33',
+  '#33ccff',
+  '#ff6666',
+  '#66ffcc',
+  '#cc99ff',
 ];
 
 // Gradient pairs
@@ -30,9 +79,19 @@ const gradients = [
   ['#ff4d4d', '#ff3399'],
   ['#ff9900', '#ffcc00'],
   ['#66ccff', '#ff3399'],
+  ['#ff0066', '#ffcc00'],
+  ['#ff9933', '#ff3399'],
+  ['#66ff99', '#00ccff'],
+  ['#cc66ff', '#ff66cc'],
+  ['#ff9966', '#66ff33'],
+  ['#ff3366', '#66ffcc'],
+  ['#99ff66', '#cc66ff'],
+  ['#ffcc66', '#ff0066'],
+  ['#33ccff', '#ff6666'],
+  ['#ff66cc', '#66ff99'],
 ];
 
-// Default shadow for all graffiti
+// Default shadow
 const defaultShadow = '4px 4px 12px rgba(0,0,0,0.4), 0 0 10px rgba(0,0,0,0.1)';
 
 const randomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
